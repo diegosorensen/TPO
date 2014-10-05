a1512bf6bdd446ca2eeaa455732cfff3880b6d6f
diegosorensen <diego.sorensen@gmail.com>
2014-10-05 19:13:20 -0300

    update con aclaraciones de ALe

--- tpo/TPO.Server/src/Entity/TareaMantenimiento.java
+++ tpo/TPO.Server/src/Entity/TareaMantenimiento.java
@@ -1,5 +1,8 @@
 package Entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.*;
 
 
@@ -14,13 +17,35 @@ public class TareaMantenimiento {
 	
 	
 	@Id
+	private int id_tarea_mantenimiento;
 	@ManyToMany
-	@JoinTable(name	="PlanMantenimiento",
+	@JoinTable(name	="PlanTareasMantenimiento",
 	joinColumns = @JoinColumn(name="id_tarea_mantenimiento"),
 	inverseJoinColumns = @JoinColumn(name="id_plan_mantenimiento"))
-	private int id_tarea_mantenimiento;
+	private List<PlanDeMantenimiento> articulos = new ArrayList<PlanDeMantenimiento>();
 	
 	private  String Descripcion;
+	
+	
+	
+	public int getId_tarea_mantenimiento() {
+		return id_tarea_mantenimiento;
+	}
+	public void setId_tarea_mantenimiento(int id_tarea_mantenimiento) {
+		this.id_tarea_mantenimiento = id_tarea_mantenimiento;
+	}
+	public String getDescripcion() {
+		return Descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		Descripcion = descripcion;
+	}
+	public int getTiempo() {
+		return Tiempo;
+	}
+	public void setTiempo(int tiempo) {
+		Tiempo = tiempo;
+	}
 	private int Tiempo ;
 
 
